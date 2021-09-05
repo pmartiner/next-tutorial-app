bbfcaad0b1460d11837d5c69f67657829e10ae80
Pablo Martínez <martipev2@hotmail.com>
2021-09-04 21:36:00 -0500

    feat(home): static content rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,16 @@ import Layout, { siteTitle } from 'components/Layout';
 import utilStyles from 'styles/utils.module.css';
 
 // Types
-import type { NextPage } from 'next';
+import type { GetStaticProps, InferGetStaticPropsType , NextPage } from 'next';
 
-const Home: NextPage = () => {
+// Utils
+import { getSortedPostsData, PostData } from 'lib/posts';
+
+type Props = {
+  allPostsData: PostData[]
+}
+
+const Home: NextPage<Props> = (props) => {
   return (
     <Layout home>
       <Head>
@@ -27,8 +34,42 @@ const Home: NextPage = () => {
           <a>over here!</a>
         </Link>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {props.allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
 }
+
+/*
+In Next.js, when you export a page component, you can also export an async function called getStaticProps. If you do this, then:
+  - getStaticProps runs at build time in production, and…
+  - Inside the function, you can fetch external data and send it as props to the page.
+
+Essentially, getStaticProps allows you to tell Next.js:
+“Hey, this page has some data dependencies — so when you pre-render this page at build time, make sure to resolve them first!”.
+NOTE: In development mode, getStaticProps runs on each request instead.
+*/
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData: PostData[] = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData
+    }
+  };
+};
 
 export default Home;
